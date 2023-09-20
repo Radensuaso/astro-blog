a810df089c31164aefb16bd96ee7c3695e268612
André Sousa <andrels9283@gmail.com>
2023-09-20 15:10:52 +0100

    create an island

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let messages: string[];
+  let currentIndex: number = -1;
+
+  let greeting = messages[0];
+
+  const randomMessage = () => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    if (randomIndex === currentIndex) {
+      randomMessage();
+    } else {
+      currentIndex = randomIndex;
+      greeting = messages[currentIndex];
+    }
+  };
+</script>
+
+<div>
+  <h3>{greeting}! Thank you for visiting!</h3>
+  <button on:click={randomMessage}> New Greeting </button>
+</div>
